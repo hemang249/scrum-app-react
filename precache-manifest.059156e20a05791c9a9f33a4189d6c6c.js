74aa5cff3cb44d004802d09e78135baef223967b
hemang <hemang249@gmail.com>
2020-05-06 16:30:07 +0530

    Updates

---
+++ precache-manifest.059156e20a05791c9a9f33a4189d6c6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe4072e754a6b59a3bf0d01fb69284b",
+    "url": "/scrum-app-react/index.html"
+  },
+  {
+    "revision": "a52f8fa9db09a1ec019f",
+    "url": "/scrum-app-react/static/css/main.ea588621.chunk.css"
+  },
+  {
+    "revision": "ca02ebfcbd8f6df84d70",
+    "url": "/scrum-app-react/static/js/2.ddb8924f.chunk.js"
+  },
+  {
+    "revision": "5c9363e977ff41d46d65dfd35cd7efcc",
+    "url": "/scrum-app-react/static/js/2.ddb8924f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52f8fa9db09a1ec019f",
+    "url": "/scrum-app-react/static/js/main.7ffda83a.chunk.js"
+  },
+  {
+    "revision": "6358979eba7525419b77",
+    "url": "/scrum-app-react/static/js/runtime-main.f3a846ac.js"
+  }
+]);
